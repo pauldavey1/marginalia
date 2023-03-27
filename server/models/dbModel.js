457f809be0d8bc797fc4ebbc9b051d85f8621f2c
Paul Davey <118549898+pauldavey1@users.noreply.github.com>
2023-03-27 19:23:34 -0400

    redefine dbModel.js for MongoDB database

--- server/models/dbModel.js
+++ server/models/dbModel.js
@@ -1,22 +1,42 @@
-const { Pool } = require('pg');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const PG_URI =
-  'postgres://wiqaswav:YrK7A0omFTHpNkjclJoLe2BwMvbqdJUc@fanny.db.elephantsql.com/wiqaswav';
+const URI =
+  'mongodb+srv://pauldavey:' +
+  process.env.MONGODB_PASSWORD +
+  '@cluster0.0nafvnn.mongodb.net/?retryWrites=true&w=majority';
 
-// create a new pool here using the connection string above
-const pool = new Pool({
-  connectionString: PG_URI,
+// connect to MongoDB
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: marginalia,
 });
 
-// table books: _id (serial), title (varchar, not null), author (varchar, not null), isbn13 (int)
-// table quotes: _id (serial), text (varchar, not null), page (int), quote (boolean), bookid (foreign key from books)
+const Schema = mongoose.Schema;
 
-// We export an object that contains a property called query,
-// which is a function that returns the invocation of pool.query() after logging the query
-// This will be required in the controllers to be the access point to the database
-module.exports = {
-  query: (text, params, callback) => {
-    console.log('executed query', text);
-    return pool.query(text, params, callback);
+const mediaSchema = new Schema({
+  id: { type: Number, required: true },
+  title: { type: String, required: true },
+  author: String,
+  isbn13: Number,
+  url: String,
+});
+
+const noteSchema = new Schema({
+  text: { type: String, required: true },
+  page: Number,
+  isQuote: { type: String, required: true },
+  mediaId: {
+    type: Schema.Types.ObjectId,
+    ref: 'media',
   },
+});
+
+const Media = mongoose.model('media', mediaSchema);
+const Note = mongoose.model('note', noteSchema);
+
+module.exports = {
+  Media,
+  Note,
 };
